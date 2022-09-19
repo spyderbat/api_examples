e9d7f9012096f4dcfb2f6d2c00e399e672ccfde0
Guy Duchatelet <guy.duchatelet@spyderbat.com>
2022-09-18 22:11:41 -0400

    support for deployment, replicasets daemonsets added

--- k8sbat/k8s_bat.sh
+++ k8sbat/k8s_bat.sh
@@ -11,14 +11,18 @@ while True
         for context in $contexts;
         do
             kubectl config use-context "$context" > /dev/null
-
-            namespaces=$(kubectl get namespaces -A -o json | tr -d '\n')
-            nodes=$(kubectl get nodes -A -o json | tr -d '\n')
-            pods=$(kubectl get pods -A -o json | tr -d '\n')
-            services=$(kubectl get services -A -o json | tr -d '\n')
-            endpoints=$(kubectl get endpoints -A -o json | tr -d '\n')
+            clusterid=$(echo $context | tr -d '/')
+            k8s_version=$(kubectl version --short | grep 'Server Version' | cut -f 2 -d : | tr -d ' ')
+            namespaces=$(kubectl get namespaces -A -o json | tr -d '\n' | tr -d ' ')
+            nodes=$(kubectl get nodes -A -o json | tr -d '\n' | tr -d ' ')
+            pods=$(kubectl get pods -A -o json | tr -d '\n' | tr -d ' ')
+            replicasets=$(kubectl get replicasets -A -o json | tr -d '\n' | tr -d ' ')
+            deployments=$(kubectl get deployments -A -o json | tr -d '\n' | tr -d ' ')
+            daemonsets=$(kubectl get daemonsets -A -o json | tr -d '\n' | tr -d ' ')
+            services=$(kubectl get services -A -o json | tr -d '\n' | tr -d ' ')
+            endpoints=$(kubectl get endpoints -A -o json | tr -d '\n' | tr -d ' ')
             time=$(date +%s)
-            echo "{\"time\": $time, \"orc_time\": $time,\"schema\": \"$schema\",\"name\": \"$context\", \"clusterid\": \"$context\", \"namespaces\": $namespaces, \"nodes\": $nodes, \"pods\": $pods, \"services\": $services, \"endpoints\": $endpoints}"
+            echo "{\"time\": $time, \"orc_time\": $time,\"schema\": \"$schema\",\"name\": \"$context\", \"k8s_version\": \"$k8s_version\", \"clusterid\": \"$clusterid\", \"namespaces\": $namespaces, \"nodes\": $nodes, \"pods\": $pods, \"replicasets\": $replicasets, \"deployments\": $deployments, \"daemonsets\": $daemonsets, \"services\": $services, \"endpoints\": $endpoints}"
         done
     sleep 60
 done
